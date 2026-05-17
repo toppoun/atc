6aa0889395f62f471223f5afc0c4f2701b9b8650
toppoun <kokisuke0000@gmail.com>
2026-05-17 22:15:37 +0900

    change gen.py

--- atc/templates/stress/gen.py
+++ atc/templates/stress/gen.py
@@ -1,5 +1,15 @@
 import random
 import sys
+import string
+
+def ni(lo: int, hi: int) -> int:
+    return random.randint(lo, hi)
+
+def nl(amount: int, lo: int, hi: int) -> None:
+    return " ".join(str(ni(lo, hi)) for _ in range(amount))
+
+def si(length: int = 1, chars: str = string.ascii_lowercase) -> str:
+    return "".join(random.choice(chars) for _ in range(length))
 
 
 def main() -> None:
@@ -7,11 +17,11 @@ def main() -> None:
     random.seed(seed)
 
     # TODO: 問題に合わせた出力に変更してください。
-    n = random.randint(1, 8)
-    a = [random.randint(0, 20) for _ in range(n)]
+    n = ni(2, 8)
+    a = nl(n, 2, 8)
 
     print(n)
-    print(*a)
+    print(a)
 
 
 if __name__ == "__main__":
